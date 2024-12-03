5ca27fff67eff9702e20a183ec43958f435ca9c3
sejda <sejdaozgur@gmail.com>
2024-12-03 22:58:43 +0300

    Home page is being created-3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import Head from "../components/Head";
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 
 export default function Home () {
   
@@ -9,7 +10,7 @@ export default function Home () {
         <>
         <Head/>
        <Header/>
-       
+       <Navbar/>
         </>
     )
 }
